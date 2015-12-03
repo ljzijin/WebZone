0d812d5347a0811a2c3af3887e9e8d24ffb2077c
ljzijin <928931647@qq.com>
2015-12-03 17:58:46 +0800

    index.html

--- weibo/views.py
+++ weibo/views.py
@@ -1,13 +1,25 @@
+#-*-coding:utf-8 -*-
 from django.shortcuts import render, render_to_response
 from django.template import Context,RequestContext
 from django.conf import settings
 from django.contrib import auth
 from models import *
 import json
-
+HOME_SECOND_NAV=[{
+	'navs':[{
+		'name':'person_page',
+		'title':u'个人主页',
+		'url':'',		
+		},{
+		'name':'friend_list',
+		'title':u'好友列表',
+		'url':'',
+		}]
+}]
 # Create your views here.
 def index(request):
 	c= RequestContext(request,{
+		'second_navs':HOME_SECOND_NAV,
 
 		})
 	return render_to_response('weibo/index.html',c)
